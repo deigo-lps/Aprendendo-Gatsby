5a853ec0603e59d6e814a18e6811f9dbd3c2b5a0
deigo-lps <diego.lopes0909@gmail.com>
2021-06-21 23:15:15 -0300

    Criando IncreaseMuscle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import Header from '../components/Header';
+import IncreaseMuscle from '../components/IncreaseMuscle';
 import '../css/main.scss'
 // markup
 export default function Index() {
   return (
     <main>
      <Header></Header>
+     <IncreaseMuscle op='1'></IncreaseMuscle>
+     <IncreaseMuscle></IncreaseMuscle>
     </main>
   );
 }
