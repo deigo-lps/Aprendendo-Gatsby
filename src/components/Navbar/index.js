6ed6a7988f4b9b3b34fe2f0543ea9bd7efd83e1e
deigo-lps <diego.lopes0909@gmail.com>
2021-06-23 01:24:17 -0300

    adicionando smooth-scroll

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,16 +3,22 @@ import { FaDumbbell } from 'react-icons/fa';
 import { FaThumbsUp } from 'react-icons/fa';
 import { FaPlay } from 'react-icons/fa';
 import { IconContext } from "react-icons";
+import { Link } from "react-scroll";
+function LinkComponent(props){
+  return (
+    <li><Link to={props.target} smooth={true} duration={700}>{props.icon} {props.children}</Link></li>
+  )
+}
 export default function Navbar() {
   return (
     <IconContext.Provider value={{ className: "navbar__icones" }}>
     <ul className="navbar">
       <li>BODYBUILDING</li>
       <ul className="navbar__content">
-        <li> <a href="#increase_muscle1"><FaDumbbell/> Increase 1</a> </li>
-        <li> <a href="#videos"><FaPlay /> Videos</a> </li>
-        <li> <a href="#increase_muscle2"><FaDumbbell/> Increase 2</a> </li>
-        <li> <a href="#reviews"><FaThumbsUp/> Reviews</a> </li>
+        <LinkComponent target="increase_muscle1" icon={<FaDumbbell/>}>Increase 1</LinkComponent>
+        <LinkComponent target="videos" icon={<FaPlay/>}>Videos</LinkComponent>
+        <LinkComponent target="increase_muscle2" icon={<FaDumbbell/>}>Increase 2</LinkComponent>
+        <LinkComponent target="reviews" icon={<FaThumbsUp/>}>Reviews</LinkComponent>
       </ul>
     </ul>
     </IconContext.Provider>
